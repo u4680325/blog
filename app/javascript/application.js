9f569384b3623d1a0135a6a064d528a8fb9e9825
Sarit Yongsiri <u4680325@gmail.com>
2026-02-08 13:38:54 +0700

    beautify code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
-import "trix"
-import "@rails/actiontext"
+import "trix";
+import "@rails/actiontext";
 
-import LocalTime from "local-time"
-LocalTime.start()
+import LocalTime from "local-time";
+LocalTime.start();
--- app/javascript/controllers/confirm_controller.js
+++ app/javascript/controllers/confirm_controller.js
@@ -1,12 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static values = { message: { type: String, default: "Are you sure?" } }
+  static values = { message: { type: String, default: "Are you sure?" } };
 
   submit(event) {
     if (!confirm(this.messageValue)) {
-      event.preventDefault()
-      event.stopPropagation()
+      event.preventDefault();
+      event.stopPropagation();
     }
   }
 }
