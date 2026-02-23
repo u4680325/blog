db227cc38d90e63635072c8e4ecc5a1c8f14a62f
Sarit Yongsiri <u4680325@gmail.com>
2026-02-23 20:41:09 +0700

    add SVG send-icon

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,18 @@ class CommentsController < ApplicationController
   before_action :set_post
 
   def create
-    @comment = @post.comments.new params.expect(comment: [ :content ])
-    @comment.user = Current.user
-    @comment.save
-    redirect_to @post
+    if params[:comment][:content].present?
+      @comment = @post.comments.new params.expect(comment: [ :content ])
+      @comment.user = Current.user
+      @comment.save
+      redirect_to @post
+    end
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_post
-    @post = Post.find(params[:post_id])
+    @post = Post.find(params.expect(:post_id))
+    raise "Cannot comment without post" if @post.nil?
   end
 end
